7b9e1d514428e91e756e6313185264636799769b
HonzaSo <jan.sobolciak@gmail.com>
2026-02-02 16:01:00 +0100

    DI - split to separate files

---
+++ SmartHome.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartHome.Application.Interfaces;
+using SmartHome.Infrastructure.Configurations;
+using SmartHome.Infrastructure.Repositories;
+
+namespace SmartHome.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureDi(this IServiceCollection services, IConfiguration configuration)
+    {
+        var databaseSection = configuration.GetSection("Database");
+    
+        services.Configure<Database>(databaseSection); 
+
+        var dbSettings = databaseSection.Get<Database>(); 
+
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            options.UseNpgsql(dbSettings?.ConnectionString, x => 
+                x.MigrationsAssembly("SmartHome.Infrastructure"));
+        });
+
+        services.AddScoped<IHomeRepository, HomeRepository>();
+
+        return services;
+    }
+}
