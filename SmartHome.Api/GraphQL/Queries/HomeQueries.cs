66198d0f67ed49531b8c7cd29af0933d21ac1a0d
HonzaSo <jan.sobolciak@gmail.com>
2026-02-03 18:31:21 +0100

    Get all homes - API - QraphQL, DI, Registration

---
+++ SmartHome.Api/GraphQL/Queries/HomeQueries.cs
@@ -0,0 +1,18 @@
+using MediatR;
+using SmartHome.Application.Queries;
+using SmartHomeApi.GraphQL.Mutations;
+using SmartHomeApi.GraphQL.Types;
+using SmartHomeApi.Mappers;
+
+namespace SmartHomeApi.GraphQL.Queries;
+
+public class HomeQueries (IMediator mediator, ILogger<HomeMutations> logger)
+{
+    public async Task<List<HomeType>> GetAllHome()
+    {
+        logger.LogInformation("Getting all homes");
+        
+        var homes = await mediator.Send(new GetAllHomesQuery());
+        return homes.Select(HomeTypeMapper.MapFromDomain).ToList();
+    }
+}
--- SmartHome.Api/GraphQL/Types/HomeType.cs
+++ SmartHome.Api/GraphQL/Types/HomeType.cs
@@ -2,6 +2,7 @@ namespace SmartHomeApi.GraphQL.Types;
 
 public class HomeType
 {
+    public Guid? Id { get; set; }
     public string Name { get; set; }
     public string Street { get; set; }
     public string City { get; set; }
