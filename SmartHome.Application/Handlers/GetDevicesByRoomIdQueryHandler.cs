958a09bbb910584dfda21bcbd42ceb68d1aa0ae0
HonzaSo <jan.sobolciak@gmail.com>
2026-02-23 20:35:55 +0100

    move check - non exist room from infra to app

--- SmartHome.Application/Handlers/GetDevicesByRoomIdQueryHandler.cs
+++ SmartHome.Application/Handlers/GetDevicesByRoomIdQueryHandler.cs
@@ -5,10 +5,16 @@ using SmartHome.Domain.Domains;
 
 namespace SmartHome.Application.Handlers;
 
-public class GetDevicesByRoomIdQueryHandler(IDeviceRepository deviceRepository) : IRequestHandler<GetDevicesByRoomIdQuery, List<Device>>
+public class GetDevicesByRoomIdQueryHandler(IDeviceRepository deviceRepository, IMediator mediator) : IRequestHandler<GetDevicesByRoomIdQuery, List<Device>?>
 {
-    public async Task<List<Device>> Handle(GetDevicesByRoomIdQuery request, CancellationToken cancellationToken)
+    public async Task<List<Device>?> Handle(GetDevicesByRoomIdQuery request, CancellationToken cancellationToken)
     {
+        var room = await mediator.Send(new GetRoomByIdQuery { Id = request.Id }, cancellationToken);
+        if (room == null)
+        {
+            return null;
+        }
+
         return await deviceRepository.GetAllByRoomIdAsync(request.Id, cancellationToken);
     }
 }
